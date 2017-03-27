0fda71acaaee214d9d6732419eaa35a9fde98810
Johannes Seidel <jojo.seidel345@web.de>
2017-03-27 14:28:49 +0200

    Push MVN Project to existing repo.

---
+++ prakt1/src/main/java/edu/hm/cs/swa/prakt1/App.java
@@ -0,0 +1,13 @@
+package edu.hm.cs.swa.prakt1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
