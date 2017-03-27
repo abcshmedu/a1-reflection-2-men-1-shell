524b62bbaa52460b888a4fd3c8cf85c9ddcaa079
Michael Reile <stimmungswecker@googlemail.com>
2017-03-27 14:52:55 +0200

    was für 1 test

--- prakt1/src/main/java/edu/hm/cs/swa/prakt1/App.java
+++ prakt1/src/main/java/edu/hm/cs/swa/prakt1/App.java
@@ -2,12 +2,9 @@ package edu.hm.cs.swa.prakt1;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Pull and push." );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Pull");
     }
 }
