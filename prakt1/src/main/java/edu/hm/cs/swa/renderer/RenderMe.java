f859c23410f27782dd0ef207b3a2ce6ba4fba927
Michael Reile <stimmungswecker@googlemail.com>
2017-04-03 10:33:42 +0200

    RenderMe überarbeitet

--- prakt1/src/main/java/edu/hm/cs/swa/renderer/RenderMe.java
+++ prakt1/src/main/java/edu/hm/cs/swa/renderer/RenderMe.java
@@ -1,10 +1,27 @@
 package edu.hm.cs.swa.renderer;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * keine ahnung warum ich das kommentieren soll.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 
 public @interface RenderMe {
 
+    /**
+     * die render Methode die das rendern ermöglicht.
+     */
+    void render();
+
+    /**
+     * die with Methode die das withen ermöglicht.
+     *
+     * @return
+     */
+    String with();
 }
