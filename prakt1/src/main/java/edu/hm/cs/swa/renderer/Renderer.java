6a3e71555ad818610dc742fac7d164aa972b02d2
Johannes Seidel <jojo.seidel345@web.de>
2017-04-06 14:42:08 +0200

    Checkstyle checked.

--- prakt1/src/main/java/edu/hm/cs/swa/renderer/Renderer.java
+++ prakt1/src/main/java/edu/hm/cs/swa/renderer/Renderer.java
@@ -8,50 +8,50 @@ import java.lang.reflect.Method;
  */
 public class Renderer {
 
-	private Object toRender;
+    private Object toRender;
 
-	/**
-	 * @param obj
-	 *            the object you want to render.
-	 */
-	public Renderer(Object obj) {
-		toRender = obj;
-	}
+    /**
+     * @param obj
+     *            the object you want to render.
+     */
+    public Renderer(Object obj) {
+        toRender = obj;
+    }
 
-	/**
-	 * @return an Object representing String.
-	 * @throws Exception
-	 *             FileNotFoundException
-	 */
-	public String render() throws Exception {
-		String tmp = "Instance of " + toRender.getClass().getName() + ":\n";
+    /**
+     * @return an Object representing String.
+     * @throws Exception
+     *             FileNotFoundException
+     */
+    public String render() throws Exception {
+        String tmp = "Instance of " + toRender.getClass().getName() + ":\n";
 
-		Method[] methods = toRender.getClass().getDeclaredMethods();
-		for (Method meth : methods) {
-			if (meth.getAnnotation(RenderMe.class) != null) {
-				meth.setAccessible(true);
+        Method[] methods = toRender.getClass().getDeclaredMethods();
+        for (Method meth : methods) {
+            if (meth.getAnnotation(RenderMe.class) != null) {
+                meth.setAccessible(true);
 
-				tmp += meth.getName() + ": " + meth.invoke(toRender) + "\n";
-			}
-		}
+                tmp += meth.getName() + ": " + meth.invoke(toRender) + "\n";
+            }
+        }
 
-		Field[] fields = toRender.getClass().getDeclaredFields();
+        Field[] fields = toRender.getClass().getDeclaredFields();
 
-		for (Field a : fields) {
-			a.setAccessible(true);
-			if (a.getAnnotation(RenderMe.class) != null) {
-				if (!a.getAnnotation(RenderMe.class).with().equals("")) {
-					tmp += a.getName() + " ";
-					Class<?> anotherRenderer = Class.forName(a.getAnnotation(RenderMe.class).with());
-					Object ott = anotherRenderer.getConstructor(Object.class).newInstance(a.get(toRender));
-					Method method = anotherRenderer.getMethod("render", null);
-					String s = (String) method.invoke(ott);
-					tmp += s;
-				} else {
-					tmp += a.getName() + " (Type " + a.getType() + "): " + a.get(toRender) + "\n";
-				}
-			}
-		}
-		return tmp;
-	}
+        for (Field a : fields) {
+            a.setAccessible(true);
+            if (a.getAnnotation(RenderMe.class) != null) {
+                if (!a.getAnnotation(RenderMe.class).with().equals("")) {
+                    tmp += a.getName() + " ";
+                    Class< ? > anotherRenderer = Class.forName(a.getAnnotation(RenderMe.class).with());
+                    Object ott = anotherRenderer.getConstructor(Object.class).newInstance(a.get(toRender));
+                    Method method = anotherRenderer.getMethod("render", (Class< ? >[]) null);
+                    String s = (String) method.invoke(ott);
+                    tmp += s;
+                } else {
+                    tmp += a.getName() + " (Type " + a.getType() + "): " + a.get(toRender) + "\n";
+                }
+            }
+        }
+        return tmp;
+    }
 }
--- prakt1/src/test/java/edu/hm/cs/swa/prakt1/RendererTest.java
+++ prakt1/src/test/java/edu/hm/cs/swa/prakt1/RendererTest.java
@@ -8,52 +8,73 @@ import java.util.Collection;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 import edu.hm.cs.swa.renderer.*;
 
+/**
+ * 
+ * @author Johannes Seidel/Michael Reile. Parameterized runner Testclass.
+ */
 @RunWith(Parameterized.class)
 public class RendererTest {
-	private SomeClass toRender;
-	private Renderer renderer;
-	private Integer inputNumber;
-	private String expectedResult;
-	
-	public RendererTest(String expectedResult, SomeClass toRender) {
-		this.renderer = new Renderer(toRender);
-		this.expectedResult = expectedResult;
-	}
-	
-//	@Before
-//	public void setUp() {
-//		toRender = new SomeClass(5);
-//		renderer = new Renderer(toRender);
-//	}
+    private Renderer renderer;
+    private String expectedResult;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
 
-	@Parameterized.Parameters
-	public static Collection<Object[]> params() {
-		return Arrays.asList(new Object[][] { {
-				"Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n" 
-						+ "foo (Type int): 0\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
-				new SomeClass(0) },
-				{
-						"Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
-								+ "foo (Type int): 1\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
-						new SomeClass(1) },
-				{ "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
-						+ "foo (Type int): 2\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
-						new SomeClass(2) },
-				{ "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
-						+ "foo (Type int): 3\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
-						new SomeClass(3) },
-				{ "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
-						+ "foo (Type int): 4\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
-						new SomeClass(4) } });
-	}
+    /**
+     * Constructor.
+     * 
+     * @param expectedResult
+     *            expected result for assertion.
+     * @param toRender
+     *            SomeClass to render.
+     */
+    public RendererTest(String expectedResult, SomeClass toRender) {
+        this.renderer = new Renderer(toRender);
+        this.expectedResult = expectedResult;
+    }
 
-	@Test
-	public void testRendering() throws Exception {
-		assertEquals(
-				expectedResult, renderer.render());
-	}
+    // @Before
+    // public void setUp() {
+    // toRender = new SomeClass(5);
+    // renderer = new Renderer(toRender);
+    // }
+
+    /**
+     * Example parameters for assertEquals.
+     * 
+     * @return ArrayList with possible answers.
+     */
+    @Parameterized.Parameters
+    public static Collection<Object[]> params() {
+        return Arrays.asList(new Object[][] { {
+                "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
+                        + "foo (Type int): 0\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                new SomeClass(0) },
+                {
+                        "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
+                                + "foo (Type int): 1\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                        new SomeClass(1) },
+                { "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
+                        + "foo (Type int): 2\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                        new SomeClass(2) },
+                { "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
+                        + "foo (Type int): 3\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                        new SomeClass(THREE) },
+                { "Instance of edu.hm.cs.swa.prakt1.SomeClass:\n" + "someMethod: 4\n"
+                        + "foo (Type int): 4\narray (Type int[]) [1, 2, 3, ]\ndate (Type class java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                        new SomeClass(FOUR) } });
+    }
+
+    /**
+     * Test for Renderer.
+     * 
+     * @throws Exception
+     *             is thrown.
+     */
+    @Test
+    public void testRendering() throws Exception {
+        assertEquals(expectedResult, renderer.render());
+    }
 }
